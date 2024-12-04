7ed618e00b230953250e011c40322d75986e36c2
Laptop-Alex <alexbgsv@gmail.com>
2024-12-04 22:06:32 +0200

    Trying stuff out

--- SantasWorkshop/base/views.py
+++ SantasWorkshop/base/views.py
@@ -3,5 +3,9 @@ from django.views.generic import TemplateView
 
 
 # Create your views here.
-class HomeView(TemplateView):
-    template_name = 'common/index.html'
+# class HomeView(TemplateView):
+#     template_name = 'common/index.html'
+
+def index(request):
+    return render(request, 'common/index.html')
+
