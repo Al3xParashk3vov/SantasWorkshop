398607016264e7bc41a3e759f41faef3b18087fa
Laptop-Alex <alexbgsv@gmail.com>
2024-12-12 21:24:13 +0200

    I FIXED THE COMA

--- SantasWorkshop/blogs/urls.py
+++ SantasWorkshop/blogs/urls.py
@@ -4,11 +4,11 @@ from SantasWorkshop.blogs import views
 
 urlpatterns = [
     path('', views.BlogPostListView.as_view(), name='post_list'),
-    path('post/new/', views.post_new, name='post_new'),
+    path('post/new/', views.BlogPostCreateView.as_view(), name='post_new'),
     path('<int:pk>', include([
         path('', views.BlogPostDetailView.as_view(), name='post_detail'),
-        path('edit/', views.post_edit, name='post_edit'),
-        path('delete/', views.post_delete, name='post_delete'),
+        path('edit/', views.BlogPostUpdateView.as_view(), name='post_edit'),
+        path('delete/', views.BlogPostDeleteView.as_view(), name='post_delete'),
     ])),
 
 ]
