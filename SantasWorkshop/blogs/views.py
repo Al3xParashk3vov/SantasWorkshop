b703fd363a970fc69db54471a11027c4497c2d97
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2024-12-13 17:15:10 +0200

    I think i am done. Now to make admin pages and try to fix style errors that bother me. And add more functions, ofcourse...

--- SantasWorkshop/blogs/views.py
+++ SantasWorkshop/blogs/views.py
@@ -9,7 +9,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.core.exceptions import PermissionDenied
 from django.contrib import messages
 
-from SantasWorkshop.blogs.forms import BlogPostForm, CommentForm
+from SantasWorkshop.blogs.forms import BlogPostForm, CommentForm, CommentFormSet
 from SantasWorkshop.blogs.models import BlogPost, Comment
 
 
@@ -32,11 +32,32 @@ class BlogPostDetailView(DetailView):
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['post'] = self.object
+    #     context['comments'] = Comment.objects.filter(post=self.object)
+    #     return context
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post'] = self.object
-        context['comments'] = Comment.objects.filter(post=self.object)
+        context['formset'] = CommentFormSet(queryset=Comment.objects.none())
         return context
+
+    def post(self, request, *args, **kwargs):
+        post = self.get_object()
+        formset = CommentFormSet(request.POST, queryset=Comment.objects.none())
+
+        if formset.is_valid():
+            for form in formset:
+                if form.cleaned_data:
+                    comment = form.save(commit=False)
+                    comment.post = post
+                    comment.save()
+            return redirect('post_detail', pk=post.pk)
+
+        context = self.get_context_data()
+        context['formset'] = formset
+        return self.render_to_response(context)
 
 
 class BlogPostCreateView(PermissionRequiredMixin, CreateView):
@@ -56,6 +77,7 @@ class BlogPostUpdateView(PermissionRequiredMixin, UpdateView):
     form_class = BlogPostForm
     template_name = 'blog/post_edit.html'
     permission_required = 'can_edit_others_posts'
+    success_url = reverse_lazy('post_list')
 
     def has_permission(self):
         obj = self.get_object()
@@ -66,6 +88,7 @@ class BlogPostDeleteView(PermissionRequiredMixin, DeleteView):
     model = BlogPost
     template_name = 'blog/post_delete.html'
     permission_required = 'can_delete_others_posts'
+    success_url = reverse_lazy('post_list')
 
     def has_permission(self):
         obj = self.get_object()
