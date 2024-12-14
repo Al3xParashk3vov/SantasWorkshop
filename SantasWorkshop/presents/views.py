28737c3d8dbfc036b6331ddfb2655d0cd8c149ab
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2024-12-14 11:41:36 +0200

    I think i am done. Now to make admin pages and try to fix style errors that bother me. And add more functions, ofcourse...

--- SantasWorkshop/presents/views.py
+++ SantasWorkshop/presents/views.py
@@ -1,36 +1,4 @@
-# from django.contrib.auth.mixins import LoginRequiredMixin
-# from django.urls import reverse_lazy
-# from django.views.generic import CreateView
-#
-# from SantasWorkshop.presents.forms import PresentAddForm
-# from SantasWorkshop.presents.models import Present
-#
-#
-# # Create your views here.
-#
-# class PresentAddPage(LoginRequiredMixin, CreateView):
-#     model = Present
-#     form_class = PresentAddForm
-#     template_name = 'presents/present-add-page.html'
-#     success_url = reverse_lazy('index')
-#
-#     def form_valid(self, form):
-#         present = form.save(commit=False)
-#         present.user = self.request.user.profile
-#         present.save()
-#         return super().form_valid(form)
-#
-#     # def get_success_url(self):
-#     #     return reverse_lazy('index')
-#
-# from django.contrib.auth.decorators import login_required
-# from django.shortcuts import render, redirect
-# from django.urls import reverse_lazy
-# from django.views.generic.edit import CreateView
-#
-# from SantasWorkshop.presents.forms import PresentAddForm
-# from SantasWorkshop.presents.models import Present
-#
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect, render
@@ -41,18 +9,6 @@ from SantasWorkshop.presents.forms import PresentAddForm
 from SantasWorkshop.presents.models import Present
 
 
-# @login_required
-# def present_add_page(request):
-#     if request.method == 'POST':
-#         form = PresentAddForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             present = form.save(commit=False)
-#             present.user = request.user
-#             present.save()
-#             return redirect('index')
-#     else:
-#         form = PresentAddForm()
-#     return render(request, 'presents/present-add-page.html', {'form': form})
 
 class PresentAddPage(LoginRequiredMixin, CreateView):
     model = Present
@@ -64,7 +20,8 @@ class PresentAddPage(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class PresentDetailView(LoginRequiredMixin, DetailView):
+# LoginRequiredMixin,
+class PresentDetailView( DetailView):
     model = Present
     template_name = 'presents/present-details-page.html'
     context_object_name = 'present'
